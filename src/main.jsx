2ac38d2b065aeee5015cf30144d22d777a8cd3bd
muscle-mentor-programs <264817325+muscle-mentor-programs@users.noreply.github.com>
2026-09-06 08:50:34 -0600

    Tighten coach workspace spacing and add light theme depth

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import './software.css'
+import './light-depth.css'
 import App from './App.jsx'
 import * as Sentry from '@sentry/react'
 
